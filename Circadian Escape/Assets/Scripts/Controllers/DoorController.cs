e7bc9fc3e8c885c2b16be92b34b295c960f0652c
Anderson Kagel <Anderson.Kagel@gmail.com>
2018-10-15 21:50:33 -0400

    Added "Abandoned Asylum" asset pack to be used in level creation. Moved some completed asset files out of my personal folders into the main ones.

---
+++ Circadian Escape/Assets/Scripts/Controllers/DoorController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorController : MonoBehaviour
+{
+    [SerializeField]
+    private string openMessage = "Press 'E' to Open";
+    [SerializeField]
+    private string closeMessage = "Press 'E' to Close";
+
+    private bool isOpen = false;
+    private bool canInteract = true;
+
+    private int animParamOpenId;
+    public float interactTime = 0.0f;
+
+    private Text actionPrompt;
+    private Animator anim;
+
+    //public Text ActionPrompt { get; private set; }
+
+    private void Start()
+    {
+        anim = gameObject.GetComponentInParent<Animator>();
+        animParamOpenId = Animator.StringToHash("Open");
+        actionPrompt = gameObject.GetComponent<Text>();
+        UpdateInteractMessage();
+	}
+	
+	private void Update()
+    {
+		//check if animation is done, reset interaction variables if it is
+        if(!canInteract && Time.time >= interactTime)
+        {
+            canInteract = true;
+            interactTime = 0.0f;
+        }
+	}
+
+    private void UpdateInteractMessage()
+    {
+        if(isOpen)
+        {
+            actionPrompt.text = closeMessage;
+        }
+
+        else
+        {
+            actionPrompt.text = openMessage;
+        }
+    }
+
+    public void Interact()
+    {
+        //only allow interaction when not animating
+        if(canInteract)
+        {
+            //toggle door state
+            isOpen = !isOpen;
+            anim.SetBool(animParamOpenId, isOpen);
+
+            //get game time when animation will be complete
+            interactTime = Time.time + anim.GetAnimatorTransitionInfo(0).duration;
+            canInteract = false;
+
+            UpdateInteractMessage();
+        }
+    }    
+}
