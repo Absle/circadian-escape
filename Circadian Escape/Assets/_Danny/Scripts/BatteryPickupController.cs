b80691a90fdc6e2e6ea9721ae7281c07f33e6f92
dgdanny05 <dgdanny05@gmail.com>
2018-10-19 16:23:44 -0400

    Changes Made.See Below:
    -duplicated andy test scene into Danny Scene folder
    -added scripts to deal with flash light/ phone
    -preliminary battery pickup with battery life tracking enabled
    - Equippable/ unequippable phone/ flash light

---
+++ Circadian Escape/Assets/_Danny/Scripts/BatteryPickupController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using StdT12;
+
+public class BatteryPickupController : MonoBehaviour, StdT12.Interfaces.IInteractable
+{
+    [SerializeField]
+    private string openMessage = "Press 'E' to Pickup Battery";
+    [SerializeField]
+    private string closeMessage = "";
+
+   // public GameObject batteryPickedUp;
+  //  private int batteries;
+
+    //interaction fields
+    private bool canInteract = true;
+  //  private float interactTime = 0.0f;
+
+    private string interactMessage = "";
+    public string InteractMessage { get { return interactMessage; } }
+    private Text actionPrompt;
+
+    //animation fields
+    private bool isPickedUp = false;
+    //  private int animParamOpenId;
+    //private Animator anim;
+
+    private int batteries;
+    public GameObject batteryPickedUp;
+
+
+    public void Start()
+    {
+        actionPrompt = gameObject.GetComponent<Text>();
+        UpdateInteractMessage();
+
+    }
+
+    private void Update()
+    {
+        //reality check to keep animator and script in sync
+        //   isOpen = anim.GetBool(animParamOpenId);
+
+        //check if animation is done, reset interaction variables if it is
+        //    if (!canInteract)
+        //   {
+        //      canInteract = true;
+        // interactTime = 0.0f;
+        //  }
+        
+    }
+
+    private void UpdateInteractMessage()
+    {
+        if (isPickedUp)
+        {
+            
+            interactMessage = closeMessage;
+        }
+
+        else
+        {
+            interactMessage = openMessage;
+        }
+
+       
+    }
+
+    public void Interact()
+    {
+        //only allow interaction when not animating
+        if (canInteract)
+        {
+            //toggle door state
+            isPickedUp = !isPickedUp;
+            //  anim.SetBool(animParamOpenId, isOpen);
+
+            //get game time when animation will be complete
+            //interactTime = Time.time + anim.GetAnimatorTransitionInfo(0).duration;
+
+            if (isPickedUp)
+            {
+                batteries += 1;
+                Destroy(batteryPickedUp);
+            }
+
+            print("batteries: " + batteries);
+            canInteract = false;
+
+            UpdateInteractMessage();
+        }
+    }
+}
---
+++ Circadian Escape/Assets/_Danny/Scripts/FlashLightBattery.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightBattery : MonoBehaviour
+{
+    public SimpleHealthBar healthBar;
+
+    private bool flashLightEnabled;
+    public GameObject flashLight;
+    public GameObject lightObject;
+    private GameObject batteryPickedUp;
+    private GameObject phonePickedUp;
+    private bool isPhoneEquipped;
+    private float usedEnergy;
+
+    public float maxEnergy;
+
+    private int batteries;
+    private float currentEnergy;
+
+
+
+    public void Start()
+    {
+        currentEnergy = maxEnergy;
+        maxEnergy = 50 * batteries;
+    }
+
+    public void FixedUpdate()
+    {
+        // 1 battery = 50 energy 
+        maxEnergy = 50 * batteries;
+        currentEnergy = maxEnergy - usedEnergy;
+        //currentEnergy = 1;
+        //equip
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            isPhoneEquipped = !isPhoneEquipped;
+
+        }
+
+        if (isPhoneEquipped)
+        {
+            flashLight.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                flashLightEnabled = !flashLightEnabled;
+            }
+
+
+            if (flashLightEnabled)
+            {
+                lightObject.SetActive(true);
+
+                if (currentEnergy <= 0)
+                {
+                    lightObject.SetActive(false);
+                    batteries = 0;
+                }
+
+                if (currentEnergy > 0)
+                {
+                    //currentEnergy = maxEnergy;
+                    lightObject.SetActive(true);
+                    //   currentEnergy -= 10.0f * Time.deltaTime;
+                    usedEnergy += 5.0f * Time.deltaTime;
+                    /// realCurrent = currentEnergy;
+                    //  print("Current Energy " + currentEnergy);
+
+
+                }
+
+                if (usedEnergy >= 50)
+                {
+                    batteries -= 1;
+                    usedEnergy = 1;
+                }
+            }
+            else
+                lightObject.SetActive(false);
+
+
+        }
+        else
+            flashLight.SetActive(false);
+
+    //    if (Input.GetKeyDown(KeyCode.F))
+     //   {
+      //      flashLightEnabled = !flashLightEnabled;
+       // }
+
+
+       
+
+        healthBar.UpdateBar(currentEnergy, maxEnergy);
+
+        print("Energy MAXIMUM  " + maxEnergy);
+        print("BATTERIES: " + batteries);
+        print("Energy Consumed  " + usedEnergy);
+        print("Current Energy " + currentEnergy);
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Battery")
+        {
+            batteryPickedUp = other.gameObject;
+            batteries += 1;
+            Destroy(batteryPickedUp);
+        }
+
+    }
+
+}
