94c3c6ba6c9c2bc583ee1c30093aec0d32f643e8
Anderson Kagel <Anderson.Kagel@gmail.com>
2018-11-03 22:05:58 -0400

    Added NavMeshComponents assets, some experimenting.

---
+++ Circadian Escape/Assets/BeastController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BeastController : MonoBehaviour
+{
+    public bool followingPlayer = true;
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private NavMeshAgent agent;
+
+    
+    // Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(followingPlayer)
+        {
+            agent.SetDestination(player.transform.position);
+        }
+	}
+}
